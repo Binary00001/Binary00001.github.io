89c34e9366c32b07aa64eda06245dca0ffc95096
binary00001 <jordansdad253@gmail.com>
2020-09-13 02:12:52 -0700

    fingers crossed

---
+++ precache-manifest.dff6e6461ed42e0e1d17af5eb583c4a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ad4e8b3bc717e2b680aea4c0a42cde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e2e01ab8129b651f035",
+    "url": "/static/css/main.40952906.chunk.css"
+  },
+  {
+    "revision": "8969ac0478f5e8ce20ef",
+    "url": "/static/js/2.83b15d77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.83b15d77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e2e01ab8129b651f035",
+    "url": "/static/js/main.9f583f40.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  }
+]);
